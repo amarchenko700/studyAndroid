ec9948e6596201bead4e9aebd5f0601984bdb89f
Alexey.Marchenko <lexx700@yandex.ru>
2021-05-24 22:01:58 +0300

    Сделана задача 3

--- app/src/main/java/com/example/lesson1/ActivityTask3.java
+++ app/src/main/java/com/example/lesson1/ActivityTask3.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityTask3 extends AppCompatActivity {
@@ -12,6 +15,17 @@ public class ActivityTask3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task3);
+
+        CalendarView myCalendarView = findViewById(R.id.myCalendarView);
+        TextView calendarInfo = findViewById(R.id.calendarInfo);
+
+        myCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String dataInfo = "Вы выбрали дату: " + dayOfMonth + "." + (month + 1) + "." + year;
+                calendarInfo.setText(dataInfo);
+            }
+        });
 
         Button backToMain = findViewById(R.id.backToMain);
         backToMain.setOnClickListener(new View.OnClickListener() {
